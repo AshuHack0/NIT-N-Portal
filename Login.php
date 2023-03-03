238aecff9d510a7909fef5de68c6f454b25fc4aa
Ashutosh Kumar <109748201+AshuHack0@users.noreply.github.com>
2023-03-03 13:00:23 +0530

    Add files via upload

---
+++ Login.php
@@ -0,0 +1,24 @@
+  <div 
+   class="container-fluid"  style="  display: flex;  flex-direction: column;  justify-content: center; align-items: center; ">
+      <form aciton="login.php" method="post"  class="loginForm"  >
+<div class="heading" >
+        <div class="text-center" >
+      <img src="https://thumbs.gfycat.com/FoolhardyFeistyAsiaticwildass-max-1mb.gif" class="" alt="..." width="150px" height="150px" style="border-radius:50%;">
+    </div>
+      <h1 class="text-center "> HEY!! Login </h1>
+    </div>
+  <div class="mb-3">
+    <label for="mobile" class="form-label" >Registration No</label>
+    <input type="number" class="form-control " id="mobile" name="mobile" aria-describedby="emailHelp" placeholder="Enter Registration No" required>
+    <div id="emailHelp" class="form-text">We'll never share your Registration no and password with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="Password" class="form-label"  >Password</label>
+    <input type="password" class="form-control" id="Password" name ="password" placeholder="Enter Your Password" required >
+  </div>
+  <button type="submit" class="btn btn-primary">Login</button>
+   <button type="buttob" class="btn btn-success" onClick="document.location.href='Registration.php'" >SignUp</button>
+   <br>
+     <br>
+   <a href="partials/_forgot.php"  style="color: red;">Forgot Your Password</a>        
+         </div>
